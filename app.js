2739c68fda05e5c599e13bea6bbfcdba1b3075a0
nateg <n8gallagher@gmail.com>
2020-12-14 12:09:18 -0800

    Express server setup and serving files to frontend

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+const path = require("path");
+
+app.use('/', express.static('frontend'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, )));
+
+// app.use('/dice', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'dice/d-twenty.obj'));
+// })
+
+
+
+app.listen(port, () => {
+  console.log(`DanceDice listening on port ${port}!`)
+})
